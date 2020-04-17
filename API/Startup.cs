5477c24f3ec0a98a85030f5098e48a2105f0754a
a-melis <amelisxx0xx@gmail.com>
2020-04-17 16:48:51 +0300

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,11 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors( options => options.AddPolicy("CorsPolicy",
+             policy => policy
+            .AllowAnyHeader()
+            .AllowAnyOrigin()
+            .WithOrigins("http://localhost:3000")));
             services.AddControllers();
         }
 
@@ -44,11 +49,12 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+            
         }
     }
 }
